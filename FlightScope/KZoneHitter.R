2d1fe7a13fc3264eed9f3c2ccd9701478a3d859b
rski4 <42653454+rski4@users.noreply.github.com>
2018-12-23 13:20:00 -0600

    Add files via upload
    Hitter K Zone

---
+++ FlightScope/KZoneHitter.R
@@ -0,0 +1,32 @@
+require(ggplot2)
+
+KZoneHitter = function(df = bp, player = "Ski ", hit = FALSE) {
+  k.zone = data.frame(
+    x1 = rep(c(-.95,-.95+(1.9/3),-.95+2*(1.9/3)), each = 3),
+    x2 = rep(c(-.95+(1.9/3),-.95+2*(1.9/3),.95), each = 3),
+    y1 = rep(c(1.6,1.6+1.9/3,1.6+2*1.9/3), 3),
+    y2 = rep(c(1.6+1.9/3,1.6+2*1.9/3,3.5), 3),
+    z = factor(c(7, 4, 1, 8, 5, 2, 9, 6, 3))
+  )
+  
+  if(hit)
+         ggplot() + ggtitle(as.character(player)) +
+           xlim(-1.5, 1.5) + xlab("") +
+           ylim(1, 4.5) + ylab("") +
+           geom_rect(data = k.zone,
+                     aes(xmin = x1, xmax = x2, ymin = y2, ymax = y1), 
+                     color = "grey20", fill = "white") +
+           geom_point(data = df[which(df$batter == player),], aes(x = px, y = pz, size = hit.ball.speed, color = hit.ball.launch.v)) +
+           scale_color_gradient(low="blue", high="red", name = "Launch Angle") +
+           scale_size(range = c(0.01,3), name = "Exit Velo")
+  else
+         ggplot() + ggtitle(as.character(player)) +
+           xlim(-1.5, 1.5) + xlab("") +
+           ylim(1, 4.5) + ylab("") +
+           geom_rect(data = k.zone,
+                     aes(xmin = x1, xmax = x2, ymin = y2, ymax = y1), 
+                     color = "grey20", fill = "white") +
+           geom_point(data = df[which(df$batter == player),], aes(x = px, y = pz))
+  
+}
+
