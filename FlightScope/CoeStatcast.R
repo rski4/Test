8bd2e218f695f4d8ec7d583a412c87f6baa74bd2
rski4 <42653454+rski4@users.noreply.github.com>
2019-01-18 07:34:31 -0600

    Update CoeStatcast.R

--- FlightScope/CoeStatcast.R
+++ FlightScope/CoeStatcast.R
@@ -4,6 +4,10 @@ require(DT)
 require(ggplot2)
 require(ggforce)
 require(RCurl)
+library(tidyverse)
+library(plotly)
+library(grid)
+library(dplyr)
 
 ui <- navbarPage("Coe Statcast",
                  
@@ -56,9 +60,9 @@ ui <- navbarPage("Coe Statcast",
                                                                   value = FALSE),
                                                     plotOutput("SprayChartBP", width = "100%")),
                                            tabPanel("K Zone", checkboxInput("KZoneHitCheckBP", "Hit Balls", value = FALSE),
-                                                    plotOutput("KZoneBP", width = "100%")),
+                                                    plotlyOutput("KZoneBP", width = "650px", height = "800px")),
                                            tabPanel("Batted Balls",
-                                                    fluidRow(column(12,plotOutput("BatBallProfileBP", width = "100%", height = "100%")),
+                                                    fluidRow(column(12,plotlyOutput("BatBallProfileBP", width = "600px", height = "800px")),
                                                     column(5, plotOutput("EVvLABP", width = "100%", height = "100%")),
                                                     column(5, offset = 2, plotOutput("LAvLDBP", width = "100%", height = "100%")))
                                                     )
@@ -91,11 +95,21 @@ ui <- navbarPage("Coe Statcast",
                                                                       choices = list("Leroy Jenkins (#99)" = "Leroy Jenkins (#99)"), selected = "Leroy Jenkins (#99)"),
                                                           mainPanel(
                                                             tabsetPanel(
+                                                              tabPanel("Dashboard", checkboxGroupInput("PitchDashBPenPitchType", 
+                                                                                                       label = h3("Pitch Type"),
+                                                                                                       choices = list("FB" = "FB",
+                                                                                                                      "CB" = "CB",
+                                                                                                                      "CU" = "CU",
+                                                                                                                      "CH" = "CH",
+                                                                                                                      "SL" = "SL"),
+                                                                                                       selected = c("FB","CB","CU","CH","SL")),
+                                                                       div(fluidRow(column(6,plotOutput("PitchDashVeloSpinBPen", width = "100%", height = "600")),
+                                                                                column(6, plotlyOutput("PitchDashKZoneBPen", width = "450px", height = "600px"))),style = 'width:1400px;')),
                                                               tabPanel("K Zone", checkboxInput("KZonePitcherBPenSpeed","Speed",
                                                                                               value = FALSE),
                                                                        checkboxInput("KZonePitcherBPenType","Pitch Type",
                                                                                      value = FALSE),
-                                                                       plotOutput("KZoneBPenPitch")),
+                                                                       plotlyOutput("KZoneBPenPitch", width = "650px", height = "800px")),
                                                               tabPanel("Movement",
                                                                        fluidRow(column(5,plotOutput("MovementBPenPFX", width = "100%", height = "100%")),
                                                                                 column(5,offset = 2, plotOutput("MovementBPen", width = "100%", height = "100%")))),
@@ -106,21 +120,25 @@ ui <- navbarPage("Coe Statcast",
                  
                  )
 
-bp = read.csv(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/BP/BP_Session.csv"), col.names = paste("col", 1:77, sep = "."))
+bp <- read.csv(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/BP/BP_Session.csv"), col.names = paste("col", 1:77, sep = "."))
 
 eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/flightscopeVar.R")))
 
-bp = flightscopeVar(bp)
+bp <- flightscopeVar(bp)
+
+bp$pitch.type <- "FB"
 
 server <- function(input, output) {
   
   eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/SprayChartFS.R")))
   
-  eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/KZoneHitter.R")))
+  eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/KZoneHitterBP.R")))
   
-  eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/HitBallProfile.R")))
+  eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/HitBallProfileBP.R")))
   
   eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/KZonePitcherBPen.R")))
+  
+  eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/PitcherDashBPen.R")))
   
   hit.leader = ddply(bp, .(batter), summarise,
                      Max.Exit.Velo = format(round(max(hit.ball.speed, na.rm = TRUE), 2), nsmall = 2),
@@ -130,7 +148,7 @@ server <- function(input, output) {
                      Carry.Avg = format(round(mean(hit.carry.dist, na.rm = TRUE), 2), nsmall = 2))
   
   output$HitLeader <- renderDataTable(
-    hit.leader[order(hit.leader$Max.Exit.Velo, decreasing = TRUE),],
+    arrange(hit.leader, desc(Max.Exit.Velo)),
     filter = 'top',
     rownames = FALSE,
     options = list(autoWidth = TRUE)
@@ -138,9 +156,9 @@ server <- function(input, output) {
   
   output$SprayChartBP <- renderPlot({SprayChartFS(player = input$'hitter', exit.velo = input$'SprayChartCheckEVBP', launch.angle = input$'SprayChartCheckLABP')}, height = 600, width = 600)
   
-  output$KZoneBP <- renderPlot({KZoneHitter(player = input$'hitter', hit = input$'KZoneHitCheckBP')}, height = 600, width = 500)
+  output$KZoneBP <- renderPlotly({KZoneHitter(player = input$'hitter', exit.velo = input$'KZoneHitCheckBP')})
   
-  output$BatBallProfileBP <- renderPlot({HitBallProfile(player = input$'hitter')}, height = 800, width = 500)
+  output$BatBallProfileBP <- renderPlotly({HitBallProfile(player = input$'hitter')})
   
   output$EVvLABP <- renderPlot(ggplot() +
                                  geom_point(data = subset(bp, batter == input$'hitter'), aes(x = hit.ball.speed, y = hit.ball.launch.v), size = 3) +
@@ -156,7 +174,7 @@ server <- function(input, output) {
                                  theme(axis.text = element_text(size = 14),
                                        axis.title = element_text(size = 16, face = "bold")), height = 500, width = 500)
   
-  output$KZoneBPenPitch <- renderPlot({KZonePitcherBPen(player = input$'player.pitch', speed = input$'KZonePitcherBPenSpeed', pitch.type = input$'KZonePitcherBPenType')}, height = 800, width = 700)
+  output$KZoneBPenPitch <- renderPlotly({KZonePitcherBPen(player = input$'player.pitch', speed = input$'KZonePitcherBPenSpeed', pitch.type = input$'KZonePitcherBPenType')})
   
   output$MovementBPenPFX <- renderPlot(ggplot() +
                                       ggtitle("Pitcher View") +
@@ -183,6 +201,13 @@ server <- function(input, output) {
                                      theme(axis.text = element_text(size = 14),
                                            axis.title = element_text(size = 16, face = "bold"),
                                            title = element_text(size = 18, face = "bold")), height = 500, width = 500)
+  
+  output$PitchDashVeloSpinBPen <- renderPlot({grid.draw(rbind(ggplotGrob(velo.bullpen(df = filter(bp, pitcher == input$'player.pitch', pitch.type == input$'PitchDashBPenPitchType'))), 
+                                                             ggplotGrob(spin.bullpen(df = filter(bp, pitcher == input$'player.pitch', pitch.type == input$'PitchDashBPenPitchType'))), 
+                                                             ggplotGrob(spin.axis.bullpen(df = filter(bp, pitcher == input$'player.pitch', pitch.type == input$'PitchDashBPenPitchType'))), 
+                                                             size = "last"))})
+  
+  output$PitchDashKZoneBPen <- renderPlotly(KZonePitcherBPen(df = filter(bp, pitch.type == input$'PitchDashBPenPitchType'), player = input$'player.pitch'))
   
 }
 
