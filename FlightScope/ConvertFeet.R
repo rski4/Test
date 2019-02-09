89d6b304571a1d15ea096e22cb773bf2ca4c4262
rski4 <42653454+rski4@users.noreply.github.com>
2019-02-09 15:36:15 -0600

    Add files via upload

---
+++ FlightScope/ConvertFeet.R
@@ -0,0 +1,24 @@
+
+ConvertFeet <- function(df){
+  char.var <- c('pitch.release.height', 
+                'pitch.release.side',
+                'pitch.extension',
+                'pitch.k.zone.height',
+                'pitch.k.zone.offset',
+                'hit.carry.dist',
+                'x0',
+                'y0',
+                'z0',
+                'px',
+                'pz')
+  for (i in char.var) {
+    df <- sapply(strsplit(as.character(live[,i]), "'|\""),
+                 function(x){ifelse(is.na(x[2]) == TRUE,
+                                    round(as.numeric(x[1]), digits = 2),
+                                    ifelse(x[1] >= 0,
+                                           round(as.numeric(x[1]) + as.numeric(x[2])/12, digits = 2),
+                                           round(as.numeric(x[1]) - as.numeric(x[2])/12, digits = 2)))})
+                                    
+  }
+  
+}
