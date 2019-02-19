340f0fa82b45c3f1d547a79c4d96b9dee470aa2a
rski4 <42653454+rski4@users.noreply.github.com>
2019-02-19 13:17:59 -0600

    Update PitcherLiveVisuals.R

--- FlightScope/PitcherLiveVisuals.R
+++ FlightScope/PitcherLiveVisuals.R
@@ -1,24 +1,6 @@
 library(RCurl)
 library(data.table)
 
-live.1 <- read.csv(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/Live/Live_2019_02_05.csv"), col.names = paste("col", 1:77, sep = "."))
-live.1$col.2 <- "2019/02/05"
-live.2 <- read.csv(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/Live/Live_02_12_2019.csv"), col.names = paste("col", 1:77, sep = "."))
-
-live <- data.frame(rbindlist(list(live.1, live.2)))
-
-eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/flightscopeVar.R")))
-
-live <- flightscopeVar(live)
-
-eval(parse(text = getURL("https://raw.githubusercontent.com/rski4/Test/master/FlightScope/ConvertFeet.R")))
-
-live <- ConvertFeet(live)
-
-live$pitch.type <- sub("^$", "No Type", live$pitch.type)
-live$pitch.type <- sub("Four Seam Fastball", "Fastball", live$pitch.type)
-live$pitch.type <- sub("Two Seam Fastball", "Fastball", live$pitch.type)
-
 pitch.symbols <- c("Fastball" = 'circle',
                    "Curveball" = 'triangle-up', 
                    "Changeup" = 'square', 
