de5bb110f2da5b3de68a5456fad3c1bde6215da5
rski4 <42653454+rski4@users.noreply.github.com>
2019-03-12 13:31:26 -0500

    Update LiveLeaderboards.R

--- FlightScope/LiveLeaderboards.R
+++ FlightScope/LiveLeaderboards.R
@@ -25,15 +25,15 @@ PlateDisciplineLeaderLive <- function(df = live) {
     dplyr::filter(!is.na(in.zone)) %>% 
     dplyr::group_by(batter) %>%
     dplyr::summarise(zSwPct = sum(swing.flag[in.zone == 1])/sum(in.zone[in.zone == 1]),
-                     zContPct = sum(contact.flag[in.zone == 1])/sum(in.zone[in.zone == 1]),
+                     zContPct = round(sum(contact.flag[in.zone == 1])/sum(in.zone[in.zone == 1]), digits = 2),
                      in.zone.n = length(in.zone[in.zone == 1]),
-                     oSwPct = sum(swing.flag[in.zone == 0])/length(in.zone[in.zone == 0]),
-                     oContPct = sum(contact.flag[in.zone == 0])/length(in.zone[in.zone == 0]),
+                     oSwPct = round(sum(swing.flag[in.zone == 0])/length(in.zone[in.zone == 0]), digits = 2),
+                     oContPct = round(sum(contact.flag[in.zone == 0])/length(in.zone[in.zone == 0]), digits = 2),
                      out.zone.n = length(in.zone[in.zone == 0]),
-                     SwPct = sum(swing.flag)/length(in.zone),
-                     ContPct = sum(contact.flag)/sum(swing.flag),
-                     ZonePct = sum(in.zone)/length(in.zone),
-                     SwStrPct = sum(swing.strike.flag)/length(in.zone),
+                     SwPct = round(sum(swing.flag)/length(in.zone), digits = 2),
+                     ContPct = round(sum(contact.flag)/sum(swing.flag), digits = 2),
+                     ZonePct = round(sum(in.zone)/length(in.zone), digits = 2),
+                     SwStrPct = round(sum(swing.strike.flag)/length(in.zone), digits = 2),
                      total.pitches = length(in.zone))
   
   return(plate.discipline.leader)
@@ -62,16 +62,16 @@ PitchPlateDisciplineLeaderLive <- function(df = live) {
   plate.discipline.leader <- live %>%
     dplyr::filter(!is.na(in.zone)) %>% 
     dplyr::group_by(pitcher) %>%
-    dplyr::summarise(zSwPct = sum(swing.flag[in.zone == 1])/sum(in.zone[in.zone == 1]),
-                     zContPct = sum(contact.flag[in.zone == 1])/sum(in.zone[in.zone == 1]),
+    dplyr::summarise(zSwPct = round(sum(swing.flag[in.zone == 1])/sum(in.zone[in.zone == 1]), digits = 2),
+                     zContPct = round(sum(contact.flag[in.zone == 1])/sum(in.zone[in.zone == 1]), digits = 2),
                      in.zone.n = length(in.zone[in.zone == 1]),
-                     oSwPct = sum(swing.flag[in.zone == 0])/length(in.zone[in.zone == 0]),
-                     oContPct = sum(contact.flag[in.zone == 0])/length(in.zone[in.zone == 0]),
+                     oSwPct = round(sum(swing.flag[in.zone == 0])/length(in.zone[in.zone == 0]), digits = 2),
+                     oContPct = round(sum(contact.flag[in.zone == 0])/length(in.zone[in.zone == 0]), digits = 2),
                      out.zone.n = length(in.zone[in.zone == 0]),
-                     SwPct = sum(swing.flag)/length(in.zone),
-                     ContPct = sum(contact.flag)/sum(swing.flag),
-                     ZonePct = sum(in.zone)/length(in.zone),
-                     SwStrPct = sum(swing.strike.flag)/length(in.zone),
+                     SwPct = round(sum(swing.flag)/length(in.zone), digits = 2),
+                     ContPct = round(sum(contact.flag)/sum(swing.flag), digits = 2),
+                     ZonePct = round(sum(in.zone)/length(in.zone), digits = 2),
+                     SwStrPct = round(sum(swing.strike.flag)/length(in.zone), digits = 2),
                      total.pitches = length(in.zone))
   
   return(plate.discipline.leader)
@@ -88,10 +88,10 @@ HitBallLeaderLive <- function(df = live) {
               EVmed = median(hit.ball.speed, na.rm = TRUE),
               Carrymax = max(hit.carry.dist, na.rm = TRUE),
               Carrymed = median(hit.carry.dist, na.rm = TRUE),
-              HardHitpct = length(hit.ball.speed[hit.ball.speed > 90])/length(hit.ball.speed),
-              LDpct = length(hit.ball.speed[hit.ball.launch.v >= 10 & hit.ball.launch.v <= 25])/length(hit.ball.launch.v),
-              FBpct = length(hit.ball.speed[hit.ball.launch.v > 25])/length(hit.ball.speed),
-              GBpct = length(hit.ball.speed[hit.ball.launch.v < 10])/length(hit.ball.speed))
+              HardHitpct = round(length(hit.ball.speed[hit.ball.speed > 90])/length(hit.ball.speed), digits = 3),
+              LDpct = round(length(hit.ball.speed[hit.ball.launch.v >= 10 & hit.ball.launch.v <= 25])/length(hit.ball.launch.v), digits = 2),
+              FBpct = round(length(hit.ball.speed[hit.ball.launch.v > 25])/length(hit.ball.speed), digits = 2),
+              GBpct = round(length(hit.ball.speed[hit.ball.launch.v < 10])/length(hit.ball.speed), digits = 2))
   
   return(Hit.Ball.Leader)
   
@@ -107,13 +107,11 @@ PitchHitBallLeaderLive <- function(df = live) {
               EVmed = median(hit.ball.speed, na.rm = TRUE),
               Carrymax = max(hit.carry.dist, na.rm = TRUE),
               Carrymed = median(hit.carry.dist, na.rm = TRUE),
-              HardHitpct = length(hit.ball.speed[hit.ball.speed > 90])/length(hit.ball.speed),
-              LDpct = length(hit.ball.speed[hit.ball.launch.v >= 10 & hit.ball.launch.v <= 25])/length(hit.ball.launch.v),
-              FBpct = length(hit.ball.speed[hit.ball.launch.v > 25])/length(hit.ball.speed),
-              GBpct = length(hit.ball.speed[hit.ball.launch.v < 10])/length(hit.ball.speed))
+              HardHitpct = round(length(hit.ball.speed[hit.ball.speed > 90])/length(hit.ball.speed), digits = 3),
+              LDpct = round(length(hit.ball.speed[hit.ball.launch.v >= 10 & hit.ball.launch.v <= 25])/length(hit.ball.launch.v), digits = 2),
+              FBpct = round(length(hit.ball.speed[hit.ball.launch.v > 25])/length(hit.ball.speed), digits = 2),
+              GBpct = round(length(hit.ball.speed[hit.ball.launch.v < 10])/length(hit.ball.speed), digits = 2))
   
   return(Hit.Ball.Leader)
   
 }
-
-
